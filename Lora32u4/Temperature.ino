cb650fa8a4efa2a38ec7cfbcfe2131161cb8ac80
Unknown <juan-2296@hotmail.es>
2018-03-31 20:00:01 -0500

    Adding SHT15 sensor

--- Lora32u4/Temperature.ino
+++ Lora32u4/Temperature.ino
@@ -1,17 +1,18 @@
 void initTemperature() {
-  sensors.begin();
+  sensors_DS.begin();
+  sht15_init();
 }
 
-int sensorTemp() {
-  sensors.requestTemperatures();
-  Serial.print(sensors.getTempCByIndex(0));
-  Serial.println(" Grados Centigrados Crudos");
-  float valueTemperature = sensors.getTempCByIndex(0);
-  int temperature = (int)round(valueTemperature);  
-  return temperature;
+float readDsSensor() {
+  sensors_DS.requestTemperatures();
+  Serial.print("DS18B20 Temp = ");
+  Serial.println(sensors_DS.getTempCByIndex(0));
+  float valueTemperature = sensors_DS.getTempCByIndex(0);
+  //int temperature = (int)round(valueTemperature);
+  return valueTemperature;
 }
 
-void printTemperature(int temperature){
+void printTemperature(int temperature) {
   String castTemperature = String(temperature);
   Serial.print(castTemperature);
   Serial.println(" Grados Centigrados Casteados");
@@ -23,4 +24,24 @@ void showTemperature(int temperature) {
   String textTemperature = castTemperature + " " +  (char)247 + "C";
   showInDisplay(3, 20 , 20, textTemperature);
 }
+void sht15_init() {
+  pinMode(power, OUTPUT);
+  pinMode(gnd, OUTPUT);
+  digitalWrite(power, HIGH);
+  digitalWrite(gnd, LOW);
+}
+
+void readSensorSTH15()
+{
+  // Read values from the sensor
+  sht_temperature = sht15.readTemperatureC();  
+  Serial.print("STH15 Temp = ");
+  Serial.print(sht_temperature);
+  Serial.println("C");
+  humidity = sht15.readHumidity();  
+  Serial.print("Humidity = ");
+  Serial.print(humidity); 
+  Serial.println("%");
+}
+
 
