1db106da0ce5634f760bd0a7acc9a4220cacb56e
Unknown <juan-2296@hotmail.es>
2018-04-09 00:32:19 -0500

    Sending information from arduino to lora

--- Arduino_Mini_Pro/Bluetooth.ino
+++ Arduino_Mini_Pro/Bluetooth.ino
@@ -1,8 +1,16 @@
-void blueToothRecieve(){
-  if(blueTooth.available()){
-   char recieved = blueTooth.read();
-   Serial.print("Received from bluetooth: ");
-   Serial.println(recieved);
+void blueToothRecieve() {
+  while (blueTooth.available()) {
+    char recieved = blueTooth.read();
+    Serial.print("Received from bluetooth: ");
+    Serial.println(recieved);
+
+    if (isDigit(recieved) && isDigit(recieved)) {
+      message += recieved;
+    }
+    
+    if (recieved == '\n' && message.length() > 0 ) {
+      sendToLora();
+    }
   }
 }
 
--- Arduino_Mini_Pro/Keypad.ino
+++ Arduino_Mini_Pro/Keypad.ino
@@ -1,7 +1,22 @@
-void keyPressed(){
-   char keypressed = myKeypad.getKey();
+
+void keyPressed() {
+  char keypressed = myKeypad.getKey();
   if (keypressed != NO_KEY)  {
-    Serial.print(keypressed);
+    //Serial.println(keypressed);
+
+    if (keypressed == 'D' && message.length() > 0) { // sending message to lora
+      sendToLora();
+    }
+
+    if (keypressed == 'C') { // clear message
+      Serial.println("clear message!");
+      message = "";
+    }
+
+    if (isDigit(keypressed)) {
+      message += keypressed;
+    }
   }
 }
+
 
