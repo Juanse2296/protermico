b35fe131554735483909caccb82e8b9e7bef1b9c
Juan Sebastián <juan-2296@hotmail.es>
2018-05-02 00:09:52 -0500

    deleting useless global variable

--- Lora32u4/Display.ino
+++ Lora32u4/Display.ino
@@ -17,6 +17,7 @@ void showInDisplay(int textSize, int x, int y, String text) {
    User flow and order interactions with the keyboard or smart phone
 */
 void steps(int action) {
+  String messageToShow = "Cantidad arecolectar...?";
   switch (action) {
     case 0:
       showInDisplay(2, 0 , 15, messageToShow);
@@ -34,12 +35,13 @@ void steps(int action) {
       showTemperature(ds_temperature);
       break;
     case 4:
-      messageToShow = "Humedad:  " + String(humidity) + " %";
+      messageToShow = "Humedad:  " + String((int)humidity) + " %";
       showInDisplay(2, 10 , 15, messageToShow );
       break;
     case 5:
       if (resultAlgorithm > 0)  {
-        showInDisplay(2, 10 , 15, "Descanso: " + String(resultAlgorithm) + " min" );
+        messageToShow = "Descanso: " + String(resultAlgorithm) + " min";
+        showInDisplay(2, 10 , 15,  messageToShow);
       } else {
         showInDisplay(2, 10 , 15, "No es     necesario descansar" );
       }
--- Lora32u4/setup.ino
+++ Lora32u4/setup.ino
@@ -1,7 +1,7 @@
 void setup()   {
-  Serial.begin(9600);
+ // Serial.begin(9600);
   Serial1.begin(9600);
-  Serial.println("Init LoRa32u4 Ra-02");
+  //Serial.println("Init LoRa32u4 Ra-02");
   initDisplay();
   initTemperature();
 }
