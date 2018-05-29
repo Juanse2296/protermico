30fd7a96ee0b887ae84ab9bf4045d8956baca51d
Unknown <juan-2296@hotmail.es>
2018-05-29 16:04:44 -0500

    .

--- Arduino_Mini_Pro/Arduino_Mini_Pro.ino
+++ Arduino_Mini_Pro/Arduino_Mini_Pro.ino
@@ -1,13 +1,11 @@
 //--------------------------------------------------LIBS
 #include <SoftwareSerial.h>
 #include <Keypad.h>
-
 #include <LiquidCrystal_I2C.h>
 
 //-------------------------------------------- DISPLAY
 
-LiquidCrystal_I2C lcd(0x27,16,2);  
-String messageToShow = "Protermico";
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 String messageToShow = "PROTERMICO,";
 String messageToShowTwo = "Bienvenido";
 char actionMessage = "";
@@ -23,26 +21,27 @@ static int constantMillis;
 unsigned long interval = 2000;
 unsigned long previousMillis = 0;
 
-//--------------------------------------------Keypad 
+//--------------------------------------------Keypad
 
-const byte numRows= 4; //number of rows on the keypad
-const byte numCols= 4; //number of columns on the keypad
+const byte numRows = 4; //number of rows on the keypad
+const byte numCols = 4; //number of columns on the keypad
 
 //keymap defines the key pressed according to the row and columns just as appears on the keypad
-char keymap[numRows][numCols]=
+char keymap[numRows][numCols] =
 {
-{'1', '2', '3', 'A'},
-{'4', '5', '6', 'B'},
-{'7', '8', '9', 'C'},
-{'*', '0', '#', 'D'}
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
 
 //Code that shows the the keypad connections to the arduino terminals
-byte rowPins[numRows] = {9,8,7,6}; //Rows 0 to 3
-byte colPins[numCols]= {5,4,3,2}; //Columns 0 to 3
+byte rowPins[numRows] = {9, 8, 7, 6}; //Rows 0 to 3
+byte colPins[numCols] = {5, 4, 3, 2}; //Columns 0 to 3
 
 //initializes an instance of the Keypad class
-Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
 //-------------------------------------------- USER FLOW
 
 int userStep = 0;
--- Arduino_Mini_Pro/Communication.ino
+++ Arduino_Mini_Pro/Communication.ino
@@ -7,15 +7,10 @@ void recieveFromLora() {
   while (serialToLora.available() > 0) {
     char recieved = serialToLora.read();
     if (isDigit(recieved))inData += recieved;
-
-
     if (recieved == '\n' && inData.length() > 0) { // read the message when user confirm using 'D'
       Serial.println(inData);
       generateMessage(actionMessage, inData);
       inData = "";
     }
-    
-    
-    
   }
 }
